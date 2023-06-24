cef6297be6a76964aa85560ffb268de72860d106
igorperic17 <igorperic@live.com>
2023-06-24 22:29:55 +0200

    First local experiment run

---
+++ main.py
@@ -0,0 +1,41 @@
+# Import the dataset from scikit-learn and create the training and test datasets. 
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_diabetes
+
+import mlflow
+import mlflow.sklearn
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+db = load_diabetes()
+X = db.data
+y = db.target
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# In this run, neither the experiment_id nor the experiment_name parameter is provided. MLflow automatically creates a notebook experiment and logs runs to it.
+# Access these runs using the Experiment sidebar. Click Experiment at the upper right of this screen. 
+with mlflow.start_run():
+  n_estimators = 100
+  max_depth = 6
+  max_features = 3
+
+  # Create and train model
+  rf = RandomForestRegressor(n_estimators = n_estimators, max_depth = max_depth, max_features = max_features)
+  rf.fit(X_train, y_train)
+
+  # Make predictions
+  predictions = rf.predict(X_test)
+  
+  # Log parameters
+  mlflow.log_param("num_trees", n_estimators)
+  mlflow.log_param("maxdepth", max_depth)
+  mlflow.log_param("max_feat", max_features)
+  
+  # Log model
+  mlflow.sklearn.log_model(rf, "random-forest-model")
+  
+  # Create metrics
+  mse = mean_squared_error(y_test, predictions)
+    
+  # Log metrics
+  mlflow.log_metric("mse", mse)
