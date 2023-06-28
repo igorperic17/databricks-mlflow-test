b6d3dce79d045fb1055e174cf02f6a1899a3e32b
igorperic17 <igorperic@live.com>
2023-06-28 17:26:35 +0200

    Implemented seasonal analysis

--- energy.py
+++ energy.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.seasonal import STL
 
 # Read the Excel file
 df = pd.read_excel('Native_Load_2022.xlsx')
 
 # Modify 'Hour Ending' column to handle "24:00" value
-df['Hour Ending'] = df['Hour Ending'].str.replace(' 24:00', ' 00:00')
+# df['Hour Ending'] = df['Hour Ending'].str.replace(' 24:00', ' 00:00')
 
 # Convert 'Hour Ending' column to datetime with error handling
 df['Hour Ending'] = pd.to_datetime(df['Hour Ending'], format='%m/%d/%Y %H:%M', errors='coerce')
@@ -30,11 +30,36 @@ target_column = 'ERCOT'
 # Remove commas and convert numeric columns to float
 df[target_column] = df[target_column].replace({',': ''}, regex=True).astype(float)
 
-# Plot the original data
-plt.plot(df['Hour Ending'], df[target_column])
+# Apply seasonal decomposition
+stl = STL(df[target_column], seasonal=23*31, period=24)  # Assuming a daily (24-hour) seasonality
+res = stl.fit()
+trend = res.trend
+seasonal = res.seasonal
+residual = res.resid
+
+# Plot the decomposed components
+plt.figure(figsize=(12, 8))
+plt.subplot(411)
+plt.plot(df['Hour Ending'], df[target_column], label='Original')
 plt.title('Original Data')
 plt.xlabel('Time')
 plt.ylabel(target_column)
+plt.subplot(412)
+plt.plot(df['Hour Ending'], trend, label='Trend')
+plt.title('Trend Component')
+plt.xlabel('Time')
+plt.ylabel('Trend')
+plt.subplot(413)
+plt.plot(df['Hour Ending'], seasonal, label='Seasonal')
+plt.title('Seasonal Component')
+plt.xlabel('Time')
+plt.ylabel('Seasonal')
+plt.subplot(414)
+plt.plot(df['Hour Ending'], residual, label='Residual')
+plt.title('Residual Component')
+plt.xlabel('Time')
+plt.ylabel('Residual')
+plt.tight_layout()
 plt.show()
 
 # Split the data into train and test sets
@@ -42,12 +67,9 @@ train_size = int(0.8 * len(df))
 train_data = df[:train_size][target_column]
 test_data = df[train_size:][target_column]
 
-# Fit an ARIMA model to the train data
-model = ARIMA(train_data, order=(1, 1, 1))
-model_fit = model.fit()
-
-# Predict the test data
-forecast = model_fit.predict(start=train_size, end=train_size + len(test_data) - 1)
+# Use the average seasonal component for prediction
+seasonal_average = seasonal[:train_size].mean()
+forecast = trend[train_size:] + seasonal_average
 
 # Calculate the root mean squared error (RMSE)
 mse = np.mean((forecast - test_data) ** 2)
